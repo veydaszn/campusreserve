8db9f6ef38de433a959ed2b3c4a34511a8bd30e3
Veydaszn <mvngone4l@gmail.com>
2025-11-17 17:52:59 +0300

    Create Resource.java

---
+++ src/models/Resource.java
@@ -0,0 +1,23 @@
+package models;
+
+
+public class Resource {
+private String id;
+private String name;
+
+
+public Resource(String id, String name) {
+this.id = id;
+this.name = name;
+}
+
+
+public String getId() { return id; }
+public String getName() { return name; }
+
+
+@Override
+public String toString() {
+return name + " (Resource ID: " + id + ")";
+}
+}
